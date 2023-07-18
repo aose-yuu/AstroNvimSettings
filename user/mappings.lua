aedfe9362a9d098dff2647566f18b20edd533ca2
aose-yuu <aose.developer@gmail.com>
2023-07-18 21:56:38 +0900

    feat: user settings directory

---
+++ user/mappings.lua
@@ -0,0 +1,43 @@
+-- Mapping data with "desc" stored directly by vim.keymap.set().
+--
+-- Please use this mappings table to set keyboard mapping since this is the
+-- lower level configuration and more robust one. (which-key will
+-- automatically pick-up stored data by this setting.)
+return {
+  -- first key is the mode
+  n = {
+    -- second key is the lefthand side of the map
+    -- mappings seen under group name "Buffer"
+    ["<leader>,"] = { "^" },
+    ["<leader>."] = { "$" },
+    ["<leader>m"] = { "%" },
+    ["<leader>fc"] = { "<cmdTelescope current_buffer_fuzzy_find<cr>", desc = "Find words in current file" },
+    ["<leader><leader>"] = { "<C-w>w" },
+    ["sv"] = { "<C-w>v", desc = "Vertical Split" },
+    ["sh"] = { "<C-w>s", desc = "Horizontal Split" },
+    ["<A-P>"] = { "<cmd>Telescope keymaps<cr>", desc = "Command pallet" },
+    ["<leader>r"] = { "<Plug>RestNvim" },
+    ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+    ["<leader>bD"] = {
+      function()
+        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(
+          bufnr) end)
+      end,
+      desc = "Pick to close",
+    },
+    -- tables with the `name` key will be registered with which-key if it's installed
+    -- this is useful for naming menus
+    ["<leader>b"] = { name = "Buffers" },
+    -- quick save
+    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+  },
+  t = {
+    -- setting a mapping to false will disable it
+    -- ["<esc>"] = false,
+  },
+  v = {
+    ["J"] = { ":move '>+1<CR>gv-gv", desc = "Move lines of code up" },
+    ["K"] = { ":move '<-2<CR>gv-gv", desc = "Move lines of code down" },
+    ["p"] = { "\"_dP" }
+  },
+}
